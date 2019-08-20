ebc0440777c756f986921e5c1ca2b90959731468
Michael David <mdavidmachacuay@gmail.com>
2019-08-20 18:22:39 -0500

    change details

--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -9,7 +9,7 @@ export class YoutubeService {
 
   private youtubeUrl: string = 'https://www.googleapis.com/youtube/v3';
   private apiKey: string = 'AIzaSyCZYVG12Rr9TfB0G-H7ZM_Qa7Hhv7xZGP8';
-  private playlist: string = 'UUuaPTYj15JSkETGnEseaFFg';
+  private playlist: string = 'UUVtSqTs5yyNySSVP5S5869w';
   private nextPageToken: string = '';
 
   constructor(
@@ -30,7 +30,7 @@ export class YoutubeService {
 
     return this.http.get(url, { params })
       .pipe(map(res => {
-        console.log(res);
+        //console.log(res);
         this.nextPageToken = res['nextPageToken'];
 
         let videos: any[] = [];
